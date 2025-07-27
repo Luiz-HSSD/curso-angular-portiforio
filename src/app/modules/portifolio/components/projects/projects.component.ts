fdba943918b9bbfd4ee96eeeee3cd8f996bb40ad
Luiz-HSSD <lhenrique_diniz@hotmail.com>
2025-07-27 00:08:45 -0300

    colocado meus projetos do curso

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -17,7 +17,7 @@ import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projec
 export class ProjectsComponent {
   #dialog = inject(MatDialog);
    public arrayProjects = signal<IProjects[]>([
-    {
+    /*{
       src:"assets/img/projects/vfull.png",
       alt:"Projeto Vida Fullstack",
       title:"Vida Fullstack",
@@ -30,7 +30,36 @@ export class ProjectsComponent {
           href:"https://vidafullstack.com.br"
         }
       ]
-    }
+    }*/
+   {
+      src:"assets/img/projects/pokedex.svg",
+      alt:"Projeto Pokedex",
+      title:"Pokedex",
+      width:"100px",
+      height:"51px",
+      description:'<p>Site da pokedex usando a Api da pokedex.</p>',
+      links:[
+        {
+          name:"Visite o site",
+          href:"https://luiz-hssd.github.io/curso-angular-pokedex/"
+        }
+      ]
+    },
+    {
+      src:"assets/img/projects/lista-de-tarefas.png",
+      alt:"Projeto Lista de Tarefas",
+      title:"Lista de Tarefas",
+      width:"100px",
+      height:"51px",
+      description:'<p>Site de uma simples lista de tarefas.</p>',
+      links:[
+        {
+          name:"Visite o site",
+          href:"https://luiz-hssd.github.io/curso-angular-lista-de-tarefas/"
+        }
+      ]
+    },
+   
    ]);
    public openDialog(data: IProjects){
     this.#dialog.open( DialogProjectsComponent , {
