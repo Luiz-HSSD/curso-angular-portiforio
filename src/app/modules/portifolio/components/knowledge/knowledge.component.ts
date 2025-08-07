6280bb6e04c9244b1b013b74005ececdc7891565
Luiz-HSSD <lhenrique_diniz@hotmail.com>
2025-08-06 23:38:00 -0300

    update customizable width height of im

--- src/app/modules/portifolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -11,30 +11,44 @@ import { IKnowledge } from '../../interface/IKnowledge.inteface';
 export class KnowledgeComponent {
  public arrayKnowledge = signal<IKnowledge[]>([
     {
+        width:"42px", 
+        height:"42px",
         src:"assets/icons/knowledge/cs.svg",
         alt:"Ícone de conhecimento C Sharp", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/angular.svg",
         alt:"Ícone de conhecimento Angular", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/css3.svg",
         alt:"Ícone de conhecimento CSS3", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/html5.svg",
         alt:"Ícone de conhecimento HTML5", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/javascript.svg",
         alt:"Ícone de conhecimento Javascript", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/nodejs.svg",
         alt:"Ícone de conhecimento Node.js", 
     },
     {
+        width:"22px", 
+        height:"24px",
         src:"assets/icons/knowledge/sass.svg",
         alt:"Ícone de conhecimento SASS", 
     }
